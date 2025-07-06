038254f67e26f949b12378b7de0dcc8a1796b1d9
Wark0711 <mickey.rahul98@gmail.com>
2025-07-06 18:58:09 +0530

    Worked on navbar and landing section

---
+++ src/components/Nabvar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { navLinks } from '../lib/utils'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
+function Nabvar() {
+
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom-top'
+            }
+        })
+
+        navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
+            backgroundColor: '#00000050',
+            backgroundFilter: 'blur(10px)',
+            duration: 1,
+            ease: 'power1.inOut'
+        });
+    })
+
+    return (
+        <nav>
+            <div>
+                <a href="#home" className='flex items-center gap-2'>
+                    <img src="/images/logo.png" alt="logo" />
+                    <p>Velvet Pour</p>
+                </a>
+
+                <ul>
+                    {
+                        navLinks.map(nav => (
+                            <li key={nav.id}>
+                                <a href={`#${nav.id}`}>{nav.title}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Nabvar
